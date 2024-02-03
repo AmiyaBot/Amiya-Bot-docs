9b1e44ac8364b24135b47bc3a8fb5ab5109e850c
Vivien <34387011+vivien8261@users.noreply.github.com>
2024-02-03 18:13:52 +0800

    Update bots.vue

--- docs/components/bots.vue
+++ docs/components/bots.vue
@@ -3,12 +3,14 @@
         <a class="title"
            href="https://qun.qq.com/qunpro/robot/qunshare?robot_uin=2854197898&robot_appid=102068219&biz_type=0">
             点击或扫码添加到群聊
-            <img src="/E0BA9D78918921391A5BE261A96C6F04.jpg" alt="">
+            <img src="https://amiyabot-1302462817.cos.ap-guangzhou.myqcloud.com/E0BA9D78918921391A5BE261A96C6F04.jpg"
+                 alt="">
         </a>
         <a class="title"
            href="https://qun.qq.com/qunpro/robot/share?robot_appid=102005657">
             点击或扫码添加到频道
-            <img src="/F7AB3AFE554027987502E5E1AB65746E.jpg" alt="">
+            <img src="https://amiyabot-1302462817.cos.ap-guangzhou.myqcloud.com/F7AB3AFE554027987502E5E1AB65746E.jpg"
+                 alt="">
         </a>
     </div>
 </template>
